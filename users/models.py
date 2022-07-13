3ea29a8e5fbba682c50756954289327b84a7a419
Kisekka-Alexander <kkalexander717@gmail.com>
2022-07-13 23:14:06 +0300

    modify user model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=50)
@@ -12,3 +13,6 @@ class User(AbstractUser):
     nationality = models.CharField(max_length=50)
     is_active = models.BooleanField(_('Active'), default=True)
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name','phone_number','date_of_birth','nationality']
