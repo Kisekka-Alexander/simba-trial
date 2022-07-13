125edf50b0273b18fb7f03b576fd0e9c363c919a
Kisekka-Alexander <kkalexander717@gmail.com>
2022-07-13 22:36:38 +0300

    create custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255,unique=True, error_messages={
+        'unique': _("A user with that email already exist.")})
+    phone_number = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    nationality = models.CharField(max_length=50)
+    is_active = models.BooleanField(_('Active'), default=True)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
